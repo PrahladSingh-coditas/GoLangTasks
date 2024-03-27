facb95ae3cf2a4d5068e764ea5361a4dfe3f4e63
PrahladSingh-coditas <158143460+PrahladSingh-coditas@users.noreply.github.com>
2024-03-27 19:32:38 +0530

    Task 2 of 27th March 2024

---
+++ perosonStruct.go
@@ -0,0 +1,45 @@
+package main
+
+import "fmt"
+
+type myFunc func(p Person) //Passing p of Person to access age  and name in func
+
+// Person Class
+type Person struct {
+	name        string
+	age         int
+	introduceMe myFunc
+}
+
+func checkVotingStatus(p Person) {
+	if p.age >= 18 {
+		fmt.Println("You are eligible to vote")
+	} else {
+		fmt.Println("You are not elegible to vote")
+	}
+}
+
+func main2() {
+
+	introduceMyself := func(p Person) {
+		fmt.Printf("Hi there, my name is %v and my age is %v\n", p.name, p.age)
+	}
+
+	//Creating an object/instance of struct
+	p1 := Person{
+		name:        "prahlad",
+		age:         15,
+		introduceMe: introduceMyself,
+	}
+	//Introduce person
+	p1.introduceMe(p1)
+
+	//check his age criteria for voting
+	checkVotingStatus(p1)
+
+	p1.age = 21
+
+	//Updated age voting status
+	checkVotingStatus(p1)
+
+}
